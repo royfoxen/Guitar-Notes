587a30c25b6f16f2fbde9a39bfe2cd9f7aaf017f
Domingas <domingas.p@yahoo.com>
2018-11-07 10:27:26 +0000

    Initial Commit

---
+++ GuitarNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuitarNotes
+//
+//  Created by Domingas on 07/11/2018.
+//  Copyright © 2018 Domingas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
